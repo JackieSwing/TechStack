6479f9a0c0a70e6267cb23c24562b2329ccf7085
JackieSwing <ldod393@163.com>
2020-02-28 14:15:05 +0800

    【特性】创建base目录，创建基于Keil-MDK5的STM32F1工程模板

---
+++ Project/C/STM32/F1/DemoBoard/USER/main.c
@@ -0,0 +1,18 @@
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+#include <stdio.h>
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+ 
+  while (1)
+  {
+  }
+}
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
